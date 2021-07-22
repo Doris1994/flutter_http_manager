bcb9509b069b89e2b2f8852591450c2184743bcb
Doris1994 <sundeqi8086@163.com>
2021-07-22 20:45:48 +0800

    fix(/):网络错误无法获取到的问题

--- lib/src/error_interceptor.dart
+++ lib/src/error_interceptor.dart
@@ -6,12 +6,13 @@ import 'request_exception.dart';
 /// 错误处理拦截器
 class ErrorInterceptor extends Interceptor {
   @override
-  void onError(DioError err,ErrorInterceptorHandler handler) {
+  void onError(DioError err, ErrorInterceptorHandler handler) {
     // error统一处理
     RequestException exception = RequestException.create(err);
     // 错误提示
     debugPrint('DioError===: ${exception.toString()}');
     err.error = exception;
-    return super.onError(err,handler);
+    handler.reject(err);
+    //super.onError(err, handler);
   }
 }
